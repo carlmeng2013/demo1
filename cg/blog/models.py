3adcdbd2c64c6327a2975d7859550c610dc0926e
carlmeng2013 <carlmeng2013@localhost.(none)>
2013-09-25 14:41:49 +0800

    add a model file

--- cg/blog/models.py
+++ cg/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=20)
+
+    def __unicode__(self):
+        return self.book_name
+
